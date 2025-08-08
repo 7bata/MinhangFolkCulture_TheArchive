5b36575585bba4155512b77307eaba8be7adc3f7
7bata <J2840864336@163.com>
2025-08-08 17:50:10 +0800

    Add test for CLIP text tokenization and remove image
    Added query_clip_test.py to test CLIP model text tokenization, including handling of truncation. Removed an unused image file from the clip directory.

---
+++ clip/clip/query_clip_test.py
@@ -0,0 +1,16 @@
+import re
+import torch
+import clip
+from PIL import Image
+import pandas as pd
+
+device = "cpu"
+model, preprocess = clip.load("ViT-B/32", device=device)
+text = "河清竹编篮是什么？"
+with torch.no_grad():
+    try:
+        query_input = clip.tokenize(text).to(device)
+        print("未截断")
+    except RuntimeError:
+        query_input = clip.tokenize(text, truncate=True).to(device)
+        print("截断")
